bee022e03ba1ed9802a6a6f0304b71ef245b19eb
vehlwn <36479428+vehlwn@users.noreply.github.com>
2023-05-24 10:55:01 +0400

    Disable record button

--- Telegram/SourceFiles/history/view/controls/history_view_compose_controls.cpp
+++ Telegram/SourceFiles/history/view/controls/history_view_compose_controls.cpp
@@ -2229,13 +2229,7 @@ void ComposeControls::orderControls() {
 }
 
 bool ComposeControls::showRecordButton() const {
-	return _features.recordMediaMessage
-		&& (_recordAvailability != Webrtc::RecordAvailability::None)
-		&& !_voiceRecordBar->isListenState()
-		&& !_voiceRecordBar->isRecordingByAnotherBar()
-		&& !HasSendText(_field)
-		&& !readyToForward()
-		&& !isEditingMessage();
+	return false;
 }
 
 bool ComposeControls::showEditStarsButton() const {
